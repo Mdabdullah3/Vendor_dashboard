4b61ae175f84987d650a6de2442df88b33ba9150
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-06-27 08:47:38 +0600

    up

---
+++ src/components/Profile/Addresset.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Addresset = () => {
+  return (
+    <section>
+      <h1 className="font-medium">Address Information</h1>
+      
+    </section>
+  );
+};
+
+export default Addresset;
--- src/page/Profile/Profile.jsx
+++ src/page/Profile/Profile.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
-import { FaUserPlus, FaEnvelope, FaDatabase } from "react-icons/fa";
+import { FaDatabase, FaRegAddressCard } from "react-icons/fa";
 import Navbar from "../../layout/Navbar";
 import { MdArrowBackIos } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
+import { RiBankCardLine } from "react-icons/ri";
 
 const Profile = () => {
-  const [activeStep, setActiveStep] = useState(0);
+  const [activeStep, setActiveStep] = useState(1);
 
   const menu = [
     { label: "Profile", icon: <CgProfile /> },
-    { label: "Address", icon: <FaUserPlus /> },
-    { label: "Verify ID & Bank", icon: <FaEnvelope /> },
+    { label: "Address", icon: <FaRegAddressCard /> },
+    { label: "Verify ID & Bank", icon: <RiBankCardLine /> },
     { label: "Add Product", icon: <FaDatabase /> },
   ];
 
@@ -21,59 +22,61 @@ const Profile = () => {
   return (
     <section className="">
       <Navbar />
-      <div>
-        <h1>
-          <MdArrowBackIos /> Back
-        </h1>
-      </div>
-      <div class="p-5 w-10/12 mx-auto">
-        <div class="mx-4 p-4">
-          <div class="flex items-center">
-            {menu.map((item, index) => (
-              <>
-                <div
-                  class={`flex items-center  relative ${
-                    activeStep === index ? "text-primary" : "text-gray-400"
-                  }`}
-                >
-                  <div
-                    class={`rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 flex justify-center cursor-pointer ${
-                      activeStep === index
-                        ? "border-primary bg-primary text-white"
-                        : "border-gray-400"
-                    }`}
-                    onClick={() => handleStepClick(index)}
-                  >
-                    {item.icon}
-                  </div>
-                  <div
-                    class={`absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase  ${
-                      activeStep === index ? "text-primary" : "text-gray-500"
-                    }`}
-                  >
-                    {item.label}
-                  </div>
-                </div>
-                {index < menu.length - 1 && (
-                  <div
-                    className={`flex-auto border-t-2 transition duration-500 ease-in-out ${
-                      activeStep === index
-                        ? "border-primary"
-                        : "border-gray-300"
-                    }`}
-                  ></div>
-                )}
-              </>
-            ))}
-          </div>
+      <div className="p-5 w-10/12 mx-auto">
+        <div>
+          <h1 className="flex items-center gap-2 mb-4">
+            <MdArrowBackIos /> Back
+          </h1>
         </div>
-        <h1 className="mt-10">
-          Please complete the todo as soon as possible, then start your business
-          journey.
-        </h1>
-      </div>
+        <div className="">
+          <div className="mx-4 p-4">
+            <div className="flex items-center">
+              {menu.map((item, index) => (
+                <>
+                  <div
+                    className={`flex items-center  relative ${
+                      activeStep === index ? "text-primary" : "text-gray-400"
+                    }`}
+                  >
+                    <div
+                      className={`rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 flex justify-center cursor-pointer ${
+                        activeStep === index
+                          ? "border-primary bg-primary text-white"
+                          : "border-gray-400"
+                      }`}
+                      onClick={() => handleStepClick(index)}
+                    >
+                      {item.icon}
+                    </div>
+                    <div
+                      className={`absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase  ${
+                        activeStep === index ? "text-primary" : "text-gray-500"
+                      }`}
+                    >
+                      {item.label}
+                    </div>
+                  </div>
+                  {index < menu.length - 1 && (
+                    <div
+                      className={`flex-auto border-t-2 transition duration-500 ease-in-out ${
+                        activeStep === index
+                          ? "border-primary"
+                          : "border-gray-300"
+                      }`}
+                    ></div>
+                  )}
+                </>
+              ))}
+            </div>
+          </div>
+          <h1 className="mt-10">
+            Please complete the todo as soon as possible, then start your
+            business journey.
+          </h1>
+        </div>
 
-      <form className="mt-6">{/* Form content here */}</form>
+        <form className="mt-6"></form>
+      </div>
     </section>
   );
 };
