13edfa97da82852e55e5e1ac8843f57c904b0607
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-09-22 21:26:52 +0600

    up

--- src/Dashboard/MessageCenter/MessageCenter.jsx
+++ src/Dashboard/MessageCenter/MessageCenter.jsx
@@ -43,8 +43,6 @@ const MessageCenter = () => {
   const formatCommentDate = (date) => {
     return formatDistanceToNow(new Date(date), { addSuffix: true });
   };
-
-  console.log(commentsWithReplies);
   return (
     <div className="container mx-auto p-6">
       <h1 className="text-3xl font-bold mb-6">Product Comments and Replies</h1>
